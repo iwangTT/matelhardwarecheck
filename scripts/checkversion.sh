6823ed264e09c1c1f41a94aef44bd00afe5729d0
Iris Wang <iwang@tenstorrent.com>
2026-03-12 09:33:13 -0700

    Enhance checkversion.sh to include more version checks
    Added commands to check various system and software versions.

--- scripts/checkversion.sh
+++ scripts/checkversion.sh
@@ -1,1 +1,24 @@
+#bash
+
+tt-smi-metal -s | grep -i "fw_bundle_version"
+
+tt-smi-metal -v
+
+modinfo tenstorrent | grep version
+
+tt-flash --version
+
+weka version
+
+lsb_release -a
+
+free -h
+
+cat /proc/meminfo
+
+systemctl status weka-agent
+
+mount | grep weka
+
+ls -la /mnt/MLPerf
 
